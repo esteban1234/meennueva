6a7b357655759589021feb8d6a1374d3e7129931
esteban1234 <tva_libra@hotmail.com>
2016-12-23 09:41:21 -0600

    pagina nueva

---
+++ js/active.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var cambio = false;
+    $('.nav li a').each(function(index) {
+        if (this.href.trim() == window.location) {
+            $(this).parent().addClass("active");
+            cambio = true;
+        }
+    });
+    if (!cambio) {
+        $('.nav li:first').addClass("active");
+    }
+});
---
+++ vistas/head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <link rel="icon" type="image/png" href="img/favicon.png" />
+      <title>
+          MEEN | CONSTRUCTORA
+      </title>
+      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+       <META NAME="Title" content="Somos una compañía ubicada en la ciudad de Mérida, Yucatán, México, dedicados a ofrecer productos para la construcción, construcción de casas, remodelación, contrucción de carreteras, edificios y ampliaciones.">
+       <META NAME="Author" CONTENT="[]"/>
+       <META name="Description" content="Somos una compañía ubicada en la ciudad de Mérida, Yucatán, México, dedicados a ofrecer productos para la construcción, construcción de casas, remodelación, contrucción de carreteras, edificios y ampliaciones."/>
+       <META NAME="Keywords" CONTENT="productos para la construcción merida yucatan, construcción merida yucatan, construcción, MEEN merida yucatan, MEEN merida yucatan, contrucción de casas merida yucatan, ampliaciones de casa merida yucatan, ingenieria civil merida yucatan, productos de construcción, levantamientos, mexico merida yucatan, desarrollo de proyectos de construcción"/>
+      <!-- <meta content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" name="viewport"> -->
+      <link href="css/estilo.css" rel="stylesheet">
+      <!-- <link href="css/estilomendina.css" rel="stylesheet"> -->
+      <link href="css/responsivo.css" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Lato|Oswald|Roboto|Roboto+Condensed" rel="stylesheet">
+      <link href="fonts/style.css" rel="stylesheet">
+    </head>
