887e9edd92090f9900a087fa61b5289560fd8c05
esteban1234 <tva_libra@hotmail.com>
2016-12-23 10:08:41 -0600

    eliminacion

--- README.md
+++
@@ -1,2 +0,0 @@
-# meennueva
-Nuevo repositorio de meen
--- vistas/Pie.php
+++
@@ -1,9 +0,0 @@
-
-
-	<footer>
-			<article class="pie">
-			<p>© 2016 <b>MEEN Constructora.</b> <span>Todos los Derechos Reservados.</span></p>
-			</article>
-	</footer>
-</body>
-</html>
