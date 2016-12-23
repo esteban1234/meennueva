4ae7b6d0cef815b6750dc3d6306ccefce3b95061
champiiy <albert1047@hotmail.com>
2016-12-23 09:48:49 -0600

    Agregando Proyecto

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php include 'vistas/head.php';?>
+<body>
+    <section class="portada">
+      <article class="reglacabeza">
+        <article class="nombrecabeza">
+          <span class="icon-text-document"></span>&nbsp;&nbsp;CONSTRUCTORA
+        </article>
+         <article class="nombrecabeza2">
+          <span class="icon-mail"></span>&nbsp;&nbsp;<b>MAIL:</b>&nbsp;&nbsp;PENDIENTE
+        </article>
+      </article>
+      <figure>
+      <img src="img/MEEN.png" alt="" width="350" height="300">
+      </figure>
+
+      <header>
+        <a href="index.php">
+          <div class="contenedor" id="tres">
+          <img class="icon" src="img/home.png">
+          <p class="texto">INICIO</p>
+          </div>
+        </a>
+
+        <a href="vistas/nosotros.php">
+          <div class="contenedor" id="cuatro">
+            <img class="icon" src="img/users.png">
+            <p class="texto">QUIENES SOMOS</p>
+          </div>
+        </a>
+
+        <a href="vistas/servicios.php">
+          <div class="contenedor" id="cinco">
+            <img class="icon" src="img/servicio.png">
+            <p class="texto">SERVICIOS</p>
+          </div>
+        </a>
+
+        <a href="vistas/contacto.php">
+          <div class="contenedor" id="seis">
+            <img class="icon" src="img/tel.png">
+            <p class="texto">CONTACTO</p>
+          </div>
+        </a>
+      </header>
+      <?php include 'vistas/Pie.php';?>
+    </section>
+    <!-- <script type="text/javascript" src="js/jquery.js"></script> -->
+    <!-- <script src="js/menu.js"></script> -->
+    <!-- <script type="text/javascript" src="js/iconoarriba.js"></script> -->
+  </body>
